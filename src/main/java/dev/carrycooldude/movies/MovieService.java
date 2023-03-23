4bad6de6817d5dd2d93a588acfcb3b3b761cae8b
carrycooldude <rawatkari554@gmail.com>
2023-03-23 20:29:23 +0530

    Added new files

--- src/main/java/dev/carrycooldude/movies/MovieService.java
+++ src/main/java/dev/carrycooldude/movies/MovieService.java
@@ -1,7 +1,16 @@
 package dev.carrycooldude.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+
+    }
 }
--- src/main/java/dev/carrycooldude/movies/MoviesApplication.java
+++ src/main/java/dev/carrycooldude/movies/MoviesApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
